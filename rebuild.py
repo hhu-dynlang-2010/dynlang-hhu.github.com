297af463cb2b072044168588c40fdafa7e2f0153
cfbolz@weizsaecker <none@none>
2010-04-07 17:58:29 +0200

    add slides for first session

--- rebuild.py
+++ rebuild.py
@@ -1,5 +1,25 @@
 import os
 
+def make_pdf(basename):
+    cmd = './rst2beamer.py %s.txt > %s.latex' % (basename, basename)
+    if os.system(cmd):
+        raise Exception
+    # amazing, isn't it?
+    cmd = "sed 's/\\\\author{}/\\\\author{Carl Friedrich Bolz, David Schneider\\\\\\\\\\nHeinrich-Heine-Universit\\\\\"at D\\\\\"usseldorf\\\\\\\\\\nSommersemester 2010}/' -i %s.latex" % (basename, )
+    print cmd
+    if os.system(cmd):
+        raise Exception
+    cmd = "pdflatex %s.latex" % (basename, )
+    if os.system(cmd):
+        raise Exception
+    cmd = "pdflatex %s.latex" % (basename, )
+    if os.system(cmd):
+        raise Exception
+    for suffix in "aux log nav out toc snm".split():
+        os.remove("%s.%s" % (basename, suffix))
+
+
+
 for dir in ['.', 'aufgaben']:
     for fn in os.listdir(dir):
         if "." not in fn:
@@ -12,10 +32,11 @@ for dir in ['.', 'aufgaben']:
             f.close()
             basename = filename[:-4]
             if '<s5defs.txt>' in data:
-                cmd = 'rst2s5.py %s.txt %s.html' % (basename, basename)
+                make_pdf(basename)
+                continue
             else:
                 cmd = 'rst2html.py --input-encoding=utf8 --output-encoding=utf8 --stylesheet-path=style.css %s.txt %s.html' % (basename, basename)
-        elif suffix == 'py' or suffix == 'pycon':
+        elif suffix == 'py' and purebasename not in ["rebuild", "rst2beamer"]:
             filename = os.path.join(dir, fn)
             cmd = 'pygmentize -l %s -o %s.html -O full,linenos,style=manni,cssfile=highlight.css %s' % (suffix, filename, filename)
         else:
