901aac64401cdcec1183b11f93fc610f60d5c7f3
David Schneider <david.schneider@picle.org>
2010-04-15 11:53:52 +0200

    updated rss feed

--- rss.py
+++ rss.py
@@ -10,6 +10,10 @@ rss = PyRSS2Gen.RSS2(
 
     items = [
        PyRSS2Gen.RSSItem(
+         title = "New room for the lecture",
+         pubDate = datetime.datetime(2011, 4, 15, 11, 42),
+         description = 'Die Vorlesung findet mittwochs um 11-13 Uhr in Raum 25.12.02.33 statt.'),
+       PyRSS2Gen.RSSItem(
          title = "Uploaded slides of lecture 1",
          pubDate = datetime.datetime(2010, 4, 14, 16, 23)),
        PyRSS2Gen.RSSItem(
