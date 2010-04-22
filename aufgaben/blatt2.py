c3e37dc220b670db41dc9e99bdb0d86f3b80f246
Carl Friedrich Bolz <cfbolz@gmx.de>
2010-04-22 09:33:28 +0200

    add blatt2

---
+++ aufgaben/blatt2.py
@@ -0,0 +1,120 @@
+import py
+
+
+# ____________________________________________________________
+#
+# AUFGABE 1
+#
+def test_word_occurences():
+    d = compute_word_occurences("a b a b a c a c")
+    assert d == {
+        "a": {"b": 2, "c": 2},
+        "b": {"a": 2},
+        "c": {"a": 1}
+    }
+
+def test_word_occurences_long():
+    f = open("faust_1")
+    s = f.read()
+    f.close()
+    d = compute_word_occurences(s)
+    assert d["FAUST:"]["Habe"] == 1
+    assert d["Habe"]["nun,"] == 1
+    assert d["nun,"]["ach!"] == 1
+    assert d["GRETCHEN:"] == {
+        'Wieso?': 1,
+        'Kein': 1,
+        'Nachbarin!': 1,
+        'Mein': 2,
+        'Meine': 1,
+        'Weh!': 1,
+        'Allm\xe4chtiger!': 1,
+        'W\xe4r': 1,
+        '(nach': 1,
+        'Das': 2,
+        'Mir': 1,
+        'Ach!': 1,
+        'Er': 1}
+
+def test_random_words():
+    f = open("faust_1")
+    s = f.read()
+    f.close()
+    d1 = compute_word_occurences(s)
+    text = make_random_text(d1, 100)
+    d2 = compute_word_occurences(text)
+    last = None
+    for word in text.split():
+        if last is not None:
+            assert word in d1[last]
+        last = word
+    print text
+
+
+# ____________________________________________________________
+#
+# AUFGABE 2
+#
+def test_mygetattr_override():
+    class A(object):
+        pass
+    register_override(object, "fortytwo", 42)
+    register_override(A, "fortyone", 41)
+    a = A()
+    assert mygetattr(a, "fortytwo") == 42
+    assert mygetattr(a, "fortyone") == 41
+    assert mygetattr([], "fortytwo") == 42
+    py.test.raises(AttributeError, mygetattr, 1, "fortyone")
+
+
+def test_override_metaclass():
+    class OverrideInt(int):
+        __metaclass__ = OverridingMetaclass
+        a = 41
+        def larger_than_fortyone(self):
+            return self > 41
+
+    assert mygetattr(1, "a") == 41
+    method = mygetattr(100, "larger_than_fortyone")
+    assert method() == True
+    method = mygetattr(1, "larger_than_fortyone")
+    assert method() == False
+
+    py.test.raises(AttributeError, mygetattr, 4.0, "a")
+
+def test_override__getattr__special_method():
+    class C(object):
+        pass
+    class OverrideC(C):
+        __metaclass__ = OverridingMetaclass
+        def __getattr__(self, attrname):
+            return len(attrname)
+    c = C()
+    assert mygetattr(c, "hello") == 5
+
+
+# ____________________________________________________________
+#
+# AUFGABE 3
+#
+
+
+def burrows_wheeler_forward(s):
+    strings = []
+    for i in range(len(s)):
+        strings.append(s[i:] + s[:i])
+    strings.sort()
+    i = strings.index(s)
+    output = ''.join([s[-1] for s in strings])
+    return (output, i)
+
+
+def test_forward_transformation():
+    # see http://de.wikipedia.org/wiki/Burrows-Wheeler-Transformation
+    assert burrows_wheeler_forward("TEXTUEL") == ("UTELXTE", 3)
+
+    input = ("TRENTATRE.TRENTINI.ANDARONO.A.TRENTO"
+             ".TUTTI.E.TRENTATRE.TROTTERELLANDO")
+    expected_output = ("OIIEEAEO..LDTTNN.RRRRRRRTNTTLE"
+                       "AAIOEEEENTRDRTTETTTTATNNTTNNAAO....OU.T")
+    assert burrows_wheeler_forward(input) == (expected_output, 60)
---
+++ aufgaben/blatt2.rst
@@ -0,0 +1,52 @@
+=======================================
+Dynamic Languages - Übungen Blatt 2
+=======================================
+
+:Abgabedatum: 13. November 2008
+:Subversion URL: svn://wyvern.cs.uni-duesseldorf.de/dynlang08
+
+
+Aufgabe 1
+---------
+
+1. Write a function that takes a string and computes a nested dictionary from it.
+   The goal is to find out which words follow another word in this string.
+   The dictionaries keys are therefore the words appearing in the string. The
+   values are again dictionaries that map the following word to a number of
+   occurrences. The test function is given in ``aufgaben/blatt1.py``.
+
+2. Such a dictionary can be used to produce a random text that is similar to the
+   text the dictionary was created from. To do this, start with a random word
+   from the dictionary. Then pick the next work out of the words that followed
+   the first word in the original text, with probabilities according to their
+   occurrences.
+
+
+Aufgabe 2
+---------
+
+1. Change ``mygettattr`` from last lecture to add a way to add "virtual"
+   attributes to types without changing the type itself, which are only seen
+   when using ``mygettattr``.  These overrides can be added with a helper
+   functions. The test functions are given in ``aufgaben/blatt2.py``. 
+
+2. Write a metaclass that exposes the overriding behaviour in a nice way. All
+   the attributes of the class are to be added to the base type of the type
+   class. The test functions are given in ``aufgaben/blatt2.py``. 
+
+
+Aufgabe 3
+---------
+
+A possible implementation of the forward part of the Burrows-Wheeler
+transformation (Aufgabe 4 Blatt 1) is given in ``blatt2.py``. However, it has
+the disadvantage that it builds in memory all shifted versions of the input
+string, which uses memory that is O(n**2) where n is the length of the input
+string. The point of this exercise is to rewrite this function to use memory
+that is only linear in the input string length.  This can be done by writing a
+class whose instances have a references to the original string and an shift
+number that indicates how much the string is shifted. The class needs a __lt__
+method that does comparisons between such instances correctly. Write tests that
+test the behaviour of this class.  In addition, the existing tests for the
+forward transformation should still pass.
+
