a425714c8baa384c52786f82e69eedfa186bc9a6
David Schneider <david.schneider@picle.org>
2010-07-28 11:15:13 +0200

    updated rss

--- rss.py
+++ rss.py
@@ -11,6 +11,9 @@ rss = PyRSS2Gen.RSS2(
 
     items = [
        PyRSS2Gen.RSSItem(
+         title = "Uploaded Solution for exercices week 10 to documentation repository",
+         pubDate = datetime.datetime(2010, 7, 28, 11, 00)),
+       PyRSS2Gen.RSSItem(
          title = "Uploaded Material for Lecture 12",
          pubDate = datetime.datetime(2010, 7, 15, 14, 12)),
        PyRSS2Gen.RSSItem(
